30a23283dec16731b4b354988b9c4eaa628f2fc2
Daniel Chong <wflogin@daniels-imac21.local>
2019-04-01 12:49:48 +0530

    Initial Commit

---
+++ SplitTabview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SplitTabview
+//
+//  Created by wflogin on 01/04/19.
+//  Copyright © 2019 Winfashion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
